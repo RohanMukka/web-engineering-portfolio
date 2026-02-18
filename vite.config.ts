f82d5b1584fb339f5f75a4ae265ddf8d9fd4aece
RohanMukka <rohanmukka09@gmail.com>
2026-02-18 13:10:29 -0600

    chore: add base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/web-engineering-portfolio/',
     server: {
       port: 3000,
       host: '0.0.0.0',
